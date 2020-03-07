1b799946f694112563a10fbde4690879faebcd39
clutso <clutso@hotmail.com>
2020-03-06 22:08:41 -0600

    payload decoder added

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	//"encoding/hex"
+	"encoding/hex"
+	"encoding/binary"
 	//"encoding/json"
 	"fmt"
 	//"io/ioutil"
@@ -19,12 +20,27 @@ const (
 	sdkClientName = "my-amazing-app"
 )
 
+//this particular application receives an array of 11 bins of uint8 numbers
+func decodePayload(payload []byte ) (float64,float64,float64) {
+
+	//mySlice := []byte{,byte(payload[3]) }
+	hSlice := []byte{0,payload[6] }
+	humedad:=float64(binary.BigEndian.Uint16(hSlice))/2
+	//binary.BigEndian.Uint32(mySlice)
+	//humedad:=0.0
+	tSlice := []byte{payload[2],payload[3] }
+	temperatura:=float64(binary.BigEndian.Uint16(tSlice))/10
+	mySlice := []byte{payload[9],payload[10] }
+	otro:=float64(binary.BigEndian.Uint16(mySlice))/100
+	return temperatura,humedad,  otro
+}
 func main (){
 
 	log := apex.Stdout() // We use a cli logger at Stdout
 	ttnlog.Set(log)      // Set the logger as default for TTN
 	appID := os.Getenv("TTN_APP_ID")
 	appAccessKey := os.Getenv("TTN_APP_ACCESS_KEY")
+	devToWatch:="fire_monitoring"
 	config := ttnsdk.NewCommunityConfig(sdkClientName)
 	config.ClientVersion = "2.0.5" // The version of the application
 	client := config.NewClient(appID, appAccessKey)
@@ -38,16 +54,37 @@ func main (){
   	log.WithError(err).Fatalf("%s: could not read CA certificate file", sdkClientName)
 	}
 
-	devices.Get("fire_monitoring")
-	dev, err = devices.Get("fire_monitoring")
+	dev, err = devices.Get(devToWatch)
 	if err != nil {
 		fmt.Println("Error getting Device")
   	log.WithError(err).Fatalf("%s: could not get device", sdkClientName)
 	}
 	fmt.Printf("got Device:%s from Application: %s and tne following description %s.\nAppEUI: %s DevEUI: %s\n",dev.DevID,dev.AppID,dev.Description, dev.AppEUI,dev.DevEUI)
 
+	pubsub, err := client.PubSub()
+	if err != nil {
+	  log.WithError(err).Fatalf("%s: could not get application pub/sub", sdkClientName)
+	}
 
+	myNewDevicePubSub := pubsub.Device(devToWatch)
 
+	uplink, err := myNewDevicePubSub.SubscribeUplink()
+	if err != nil {
+  	log.WithError(err).Fatalf("%s: could not subscribe to uplink messages", sdkClientName)
+	}
+	go func() {
+  	for message := range uplink {
+    	hexPayload := hex.EncodeToString(message.PayloadRaw)
+			fmt.Println( "Received uplink with values:")
+			temperatura, humedad,otro:= decodePayload (message.PayloadRaw)
+			fmt.Printf("Temperatura: %2f, Humedad: %2f , no me acuerdo como se llama: %2f\n", temperatura, humedad,otro)
+			log.WithField("data", hexPayload).Infof("%s: received uplink", sdkClientName)
+
+  	}
+	}()
+for{
+
+}
 /*
 	dev.AppID = appID
 	dev.DevID = "fire_monitoring"
@@ -56,4 +93,5 @@ func main (){
 	dev.DevEUI = types.DevEUI{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08} // Use the real DevEUI here
 	random.FillBytes(dev.AppKey[:])
 */
+
 }
