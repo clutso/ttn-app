501e7272e0ad59ee04663840ac48218a3c2d7dac
clutso <clutso@hotmail.com>
2020-03-16 19:49:03 -0600

    js folder and files created to organize js routines

--- httpServer/static/js/GenericFunctions.js
+++ httpServer/static/js/GenericFunctions.js
@@ -4,6 +4,16 @@ function sayHI(){
   alert("HI")
 }
 
+
+function validateForm() {
+if (document.forms[0]["Name"].value == "" || document.forms[0]["Email"].value == "" || document.forms[0]["Subject"].value == "" ||document.forms[0]["Message"].value == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+document.forms[0].submit()
+alert("Email Sent");
+}
+
 setInterval(function(){
   updtDash();
 }, 10000);
--- httpServer/static/js/MapFunctions.js
+++ httpServer/static/js/MapFunctions.js
@@ -6,8 +6,6 @@ var lat;
 var lng;
 
 function myMap() {
-
-//  myLatlng = new google.maps.LatLng(21.823211,-102.284552);
   lat = parseFloat(document.getElementById('lat').textContent);
   lng = parseFloat(document.getElementById('lng').textContent);
   myLatlng=new google.maps.LatLng(lat,lng);
@@ -22,17 +20,3 @@ function myMap() {
   marker = new google.maps.Marker({position: myLatlng,title:"Monitor here"});
   marker.setMap(map);
   }
-/*
-function updtLocation() {
-    lat = parseFloat(document.getElementById('lat').textContent);
-    lng = parseFloat(document.getElementById('lng').textContent);
-    myLatlng=new google.maps.LatLng(lat,lng);
-    mapOptions = {
-      zoom: 17,
-      center: myLatlng
-      }
-    map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-    marker = new google.maps.Marker({position: myLatlng,title:"Monitor here"});
-    marker.setMap(map);
-    }
-*/
